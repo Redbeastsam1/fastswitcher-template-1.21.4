799391b90af683d86b0c1e532963113813e5892f
Redbeastsam1 <redbeastsam@gmail.com>
2025-08-25 03:59:26 +0530

    Started the project......

---
+++ src/main/java/net/redbeastsam/fastswitcher/FastSwitcherClient.java
@@ -0,0 +1,10 @@
+package net.redbeastsam.fastswitcher;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FastSwitcherClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
